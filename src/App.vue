26e4c838f83f0bb44ca9c6c720113442b9a25f7e
Derian Pinto <pintoderian@gmail.com>
2022-03-14 10:21:29 -0500

    Creando componente de Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <p>
-      <router-link to="/">Inicio</router-link>
-      <router-link to="/search">Buscador</router-link>
-    </p>
-    <router-view />
+    <Navbar />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
 export default {
   name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
@@ -19,8 +22,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-5">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand to="/">Polimundo</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Inicio</b-nav-item>
+          <b-nav-item to="/search">Resultados</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
